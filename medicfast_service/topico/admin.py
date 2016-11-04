6b62ce6236065c4fa989c7af7faec9b664852a67
ulises <ulises.lch1@gmail.com>
2016-11-04 04:00:55 -0500

    8

--- medicfast_service/topico/admin.py
+++ medicfast_service/topico/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from topico.models import Historialmedic
 # Register your models here.
+
+
+class HistorialmedicAdmin(admin.ModelAdmin):
+    list_display = ("fecha", "hora", "numhistorial")
+
+    search_fields = ("fecha", "hora", "numhistorial")
+    list_per_page = 2
+
+admin.site.register(Historialmedic, HistorialmedicAdmin)
--- medicfast_service/topico/models.py
+++ medicfast_service/topico/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Historialmedic(models.Model):
+
+    fecha = models.DateField(null=True, blank=True)
+    hora = models.DateTimeField(auto_now=True)
+    numhistorial = models.CharField(max_length=60, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Historialmedic"
+        verbose_name_plural = "Historialmedics"
+
+    def __str__(self):
+        return self.numhistorial
